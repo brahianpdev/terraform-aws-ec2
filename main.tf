83fd21397dc2a1759623e94d5769c9a40a2711e0
brahianpdev <zk.spywn@gmail.com>
2022-10-12 20:12:28 -0300

    fix(main): modularized changes

--- main.tf
+++ main.tf
@@ -1,37 +1,11 @@
-provider "aws" {
-    region = "us-east-1"
-
-    // Credentials from ~/.aws/credentials (IAM user)
-    access_key = ""
-    secret_key = "" 
-}
-
-data "aws_ami" "ubuntu" {
-    most_recent = true
-
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
-    }
-
-    filter {
-        name   = "virtualization-type"
-        values = ["hvm"] // hardware virtual machine (recommended)
-    }
-
-    owners = ["099720109477"] // Canonical
-}
-
 resource "aws_instance" "web" {
-    ami            =  data.aws_ami.ubuntu.id 
-    instance_type  =  "t2.micro"
-    key_name       =  "aws-key-pair"
-
-    tags = {
-        Name = "web-server"
-    }
+  ami             = data.aws_ami.ubuntu.id
+  instance_type   = var.instance_type
+  tags = {
+    Name = "ec2-instance"
+  }
 }
 
 output "public_ip" {
-    value = aws_instance.web.public_ip
+  value = aws_instance.web.public_ip
 }
