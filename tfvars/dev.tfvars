c652711fc81eb92928c4357f0d9557bfcf143b3a
brahianpdev <zk.spywn@gmail.com>
2022-10-13 09:53:00 -0300

    fix(tf): remove unnecesary code

--- tfvars/dev.tfvars
+++
@@ -1,4 +0,0 @@
-app_env            = "dev"
-workspace          = "dev"
-region             = "us-east-1"
-keypair_name       = "YOU KEYPAIR NAME.pem"
--- variables.tf
+++ variables.tf
@@ -1,15 +1,3 @@
-locals {
-  common_tags = {
-    environment = var.app_env
-  }
-}
-
-variable "app_env" {
-  description = "The environment of the application"
-  type = string
-  default = "dev"
-}
-
 variable "aws_region" {
   description = "AWS region for all resources."
   type    = string
@@ -19,7 +7,7 @@ variable "aws_region" {
 variable "keypair_name" {
     description = "Name of the keypair to use for the instances."
     type    = string
-    default   = "YOU KEYPAIR NAME.pem"
+    default   = "PATH TO YOUR KEYPAIR"
 }
 
 variable "instance_type" {
